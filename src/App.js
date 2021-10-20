155cbcea563406297764becf54f93e1c08643d15
David Weisberg <davidweisbergacea@gmail.com>
2021-10-19 23:58:26 -0400

    Second commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 
 function App() {
   return (
-    <MapContainer center={[45.4, -75.7]} zoom={8}>
+    <MapContainer center={[48.148598, 17.107748]} zoom={5}>
       <TileLayer
         url="https://api.mapbox.com/styles/v1/dweisb/ckuyvslt608od14o4h0cqpblf/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZHdlaXNiIiwiYSI6ImNrdGQzZzQ2aTBicGEyb3BoZjI1YjNwaGkifQ.9LTCnUZDmffDzf7BzqVq5w"
         attribution='Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
