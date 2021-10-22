b22566f2dad8ab4d54a7b7686d77d39d3650592e
David Weisberg <davidweisbergacea@gmail.com>
2021-10-21 21:32:56 -0400

    Popup changes when new gadol clicked

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Icon } from 'leaflet';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import "./App.css";
+import Navigator from './components/Navigator.js';
+
+
+
+
 
 function App() {
+  const [numOfCardNotShown, setGadolCard] = React.useState(0);
+  const [map, setMap] = useState(null);
+  const [currentGadol, setCurrentGadol] = useState('');
+
+
+  const getGadol = (gadol) => {
+    setCurrentGadol(gadol);
+  }
+
   return (
-    <MapContainer center={[48.148598, 17.107748]} zoom={5}>
+    <div>
+      <Navigator getGadol = {getGadol} />
+    <MapContainer center={[48.148598, 17.107748]} zoom={5} whenCreated={setMap}>
       <TileLayer
         url="https://api.mapbox.com/styles/v1/dweisb/ckuyvslt608od14o4h0cqpblf/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoiZHdlaXNiIiwiYSI6ImNrdGQzZzQ2aTBicGEyb3BoZjI1YjNwaGkifQ.9LTCnUZDmffDzf7BzqVq5w"
         attribution='Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>'
       />
-      </MapContainer>
+      
+    <Marker position={currentGadol.Position}><Popup> {currentGadol.Name} </Popup></Marker>
+     {/* { data.map((gadol) => {return <Marker position={gadol.Position}><Popup> {gadol.Name} </Popup></Marker>;})}  */}
+     
+        </MapContainer>
+        
+
+         
+        </div>
+       
   );
 }
 
---
+++ src/components/Navigator.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import { LeafletEventHandlerFnMap, Map } from 'leaflet';
+import './Navigator.css'
+import {Tab, Tabs, ListGroup} from 'react-bootstrap'
+import { MapContainer, TileLayer, Marker, Popup, useMap} from 'react-leaflet';
+
+import NavigatorTab from './NavigatorTab'
+import '../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+const data = [
+  {
+    "Name": "Rashi",
+    "City": "Troyes",
+    "Position": [48.29752, 4.07471],
+    "Year Born": 1040,
+    "Year Died": 1105,
+    "Teachers": "",
+    "Students": ""
+  },
+  {
+    "Name": "Rif",
+    "City": "Fez",
+    "Position": [34.059769, -4.96531],
+    "Year Born": 1013,
+    "Year Died": 1103,
+    "Teachers": "",
+    "Students": ""
+  },
+  {
+    "Name": "Rambam",
+    "City": "Cordova",
+    "Position": [37.88472, -4.77913],
+    "Year Born": 1138,
+    "Year Died": 1205,
+    "Teachers": "",
+    "Students": ""
+  },
+  {
+    "Name": "Aruch",
+    "City": "Rome",
+    "Position": [41.902782, 12.496365],
+    "Year Born": 1045,
+    "Year Died": 1103,
+    "Teachers": "",
+    "Students": ""
+  },
+  {
+    "Name": "Rabeinu Tam",
+    "City": "Ramerupt",
+    "Position": [48.51856, 4.2938],
+    "Year Born": 1100,
+    "Year Died": 1171,
+    "Teachers": "",
+    "Students": ""
+  },
+  {
+    "Name": "Ibn Ezra",
+    "City": "Cordova",
+    "Position": [37.88472, -4.77913],
+    "Year Born": 1089,
+    "Year Died": 1164,
+    "Teachers": "",
+    "Students": ""
+  }
+]
+
+function Navigator({getGadol}) {
+
+     return (
+
+<div className= "navigator">
+      <h4 >GadolMaps!!</h4>
+      <Tabs defaultActiveKey="first">
+        <Tab  eventKey="first" title="Rishonim">   <ListGroup> {data.map((gadol) => {
+              return  <ListGroup.Item action onClick={ () => getGadol(gadol)}>{gadol.Name}</ListGroup.Item>})}
+      </ListGroup>
+        
+        </Tab>
+        <Tab eventKey="second" title="Achronim">
+          Hii, I am 2nd tab content
+        </Tab>
+        </Tabs>
+        
+     
+
+        </div>
+    )
+}
+
+export default Navigator;
